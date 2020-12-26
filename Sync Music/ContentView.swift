a058c868a7fcba8aa495a6b27857f8ef201efdb2
MaxenceMottard <mottard.maxence@gmail.com>
2020-12-26 10:32:49 +0100

    Initial Commit

---
+++ Sync Music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sync Music
+//
+//  Created by Maxence Mottard on 26/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sync Music/Sync_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sync_MusicApp.swift
+//  Sync Music
+//
+//  Created by Maxence Mottard on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Sync_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
